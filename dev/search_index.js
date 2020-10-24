bb34913c64ac9ce647378bab2ec30e21beccb018
Documenter.jl <documenter@juliadocs.github.io>
2020-10-24 06:13:19 +0000

    build based on 32ffecf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"examples/example1/#an-example","page":"Example 1","title":"an example","text":"","category":"section"},{"location":"installation/#Installation","page":"Installation","title":"Installation","text":"","category":"section"},{"location":"#testseml.jl","page":"Home","title":"testseml.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for testseml.jl","category":"page"}]
+}
